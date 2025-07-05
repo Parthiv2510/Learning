d0035d2ea06aaccd3f672bb3a05a23eb3f245a35
parthiv2510 <parthivkm73@gmail.com>
2025-07-06 02:38:01 +0530

    1st commit

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ECs!"
+    return "Hello from ECs  task done!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
